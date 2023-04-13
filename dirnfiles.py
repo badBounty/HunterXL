ae4711d054309e7c46c0870f1ccd7158acceea62
nicovarvara <nicovarvara@gmail.com>
2023-04-13 11:22:10 -0400

    Add existing file

---
+++ dirnfiles.py
@@ -0,0 +1,17 @@
+import os
+
+OUTPATH_SUBDOMAINS_WEBAPP="outputs/subdomains-webapp.txt"
+OUTPATH_VULNERABILITIES="outputs/vulnerabilities.txt"
+OUTPATH_DIRNFILES_GAU_AUX="outputs/dirnfiles-gau-aux.txt"
+OUTPATH_DIRNFILES="outputs/dirnfiles.txt"
+
+if(os.path.isfile(OUTPATH_SUBDOMAINS_WEBAPP)):
+	webappsList=open(OUTPATH_SUBDOMAINS_WEBAPP, 'r')
+	while True:
+		endpoint=webappsList.readline()
+		if not endpoint:
+			break
+		os.system("gau "+endpoint.strip()+" --verbose --o "+OUTPATH_DIRNFILES_GAU_AUX)
+		os.system("sort -u "+OUTPATH_DIRNFILES_GAU_AUX+" >> "+OUTPATH_DIRNFILES)
+		os.remove(OUTPATH_DIRNFILES_GAU_AUX)
+	webappsList.close()
---
+++ protocols.py
@@ -0,0 +1,31 @@
+import subprocess,os,shutil
+
+from os import system
+
+DIRPATH_OUTPUTS="outputs"
+
+OUTPATH_SUBDOMAINS="outputs/subdomains.txt"
+OUTPATH_SUBDOMAINS_WEBAPP_AUX="outputs/subdomains-webapp-aux.txt"
+OUTPATH_SUBDOMAINS_WEBAPP="outputs/subdomains-webapp.txt"
+
+system("cat " +OUTPATH_SUBDOMAINS+" | aquatone -ports large -threads 7 -chrome-path /usr/bin/chromium")
+system("cat aquatone_urls.txt | grep 'https:' > "+OUTPATH_SUBDOMAINS_WEBAPP_AUX)
+os.remove("aquatone_urls.txt")
+os.remove("aquatone_report.html")
+os.remove("aquatone_session.json")
+shutil.rmtree("headers")
+shutil.rmtree("html")
+shutil.rmtree("screenshots")
+
+subdomainsList=open(OUTPATH_SUBDOMAINS_WEBAPP_AUX, 'r')
+while True:
+	subdomain=subdomainsList.readline()
+	if not subdomain:
+		break
+	statusCode = subprocess.run(['curl', subdomain.strip(), '-o', '/dev/null', '-s', '-w', '%{http_code}'], check=False, capture_output=True, text=True).stdout
+	if statusCode == "200":
+		system("echo "+subdomain.strip()+" >> "+OUTPATH_SUBDOMAINS_WEBAPP)
+subdomainsList.close()
+os.remove(OUTPATH_SUBDOMAINS_WEBAPP_AUX)
+
+
