71b81c4326552d585d41c3acbda4425c1d2f6859
Maxpowersi <maxi1990@gmail.com>
2023-06-01 19:41:36 -0300

    Update vulnerabilities.py

--- vulnerabilities.py
+++ vulnerabilities.py
@@ -1,28 +1,16 @@
-import json,os,subprocess
+import os,subprocess
 
-OUTPATH_SUBDOMAINS_WEBAPP="outputs/subdomains-webapp.txt"
-OUTPATH_NUCLEI="outputs/nuclei.json"
-OUTPATH_TESTSSL="outputs/output_ssl.html"
-OUTPATH_SUBDOMAINS="outputs/subdomains.txt"
-OUTPATH_SSLSCAN="outputs/output_sslscan.txt"
-OUTPATH_NIKTO="outputs/output_nikto.txt"
-OUTPATH_ZAP="outputs/zap-output.csv"
-#os.system("nuclei -l "+OUTPATH_SUBDOMAINS_WEBAPP+" -t /home/admin/nuclei-templates -je -o "+OUTPATH_NUCLEI)
-#if [os.path.exists(OUTPATH_TESTSSL)]:
-#	(os.remove(OUTPATH_TESTSSL))
-#else:
-#	subprocess.run(["./tools/testssl.sh/testssl.sh", "--htmlfile", OUTPATH_TESTSSL, "-iL", OUTPATH_SUBDOMAINS_WEBAPP])
+import subprocess,os
 
-#os.system("sslscan --targets="+OUTPATH_SUBDOMAINS_WEBAPP+" > "+OUTPATH_SSLSCAN)
-#os.system("nikto -host "+OUTPATH_SUBDOMAINS_WEBAPP+" -output "+OUTPATH_NIKTO)
+INPATH_WEBSUBDOMAINS="subdomains-webapp.txt"
+INPATH_SPIDERING="spidering.txt"
 
-#Zap en docker
-comando = "docker run -v $(pwd):/zap/wrk --rm owasp/zap2docker-stable zap-baseline.py -t http://torneos.com -x OUTPATH_ZAP"
+WGET="https://gist.github.com/maxpowersi/4c8282e16ac5149acf1b7409aa44b511/raw/a0bf379ac1a65805fca00aacfe689e5d5e5a0302/vulners.sh"
+VUL_NAME="vulners.sh"
 
-# Ejecutar el comando con sudo
-sudo_comando = ["sudo", "bash","-c", comando]
+PINGBACK="http://pingb.in/p/78d27164f0d7b2b00ca80599dcad"
 
-# Llamar al comando con subprocess
-subprocess.run(sudo_comando)
-
+subprocess.run(["wget", WGET])
+subprocess.run(["sh", VUL_NAME, INPATH_WEBSUBDOMAINS, INPATH_SPIDERING, PINGBACK])
+subprocess.run(["rm", VUL_NAME])
 
