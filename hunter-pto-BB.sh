e48547863e6669f037af1ee37666dd28c468e1df
Maxpowersi <maxi1990@gmail.com>
2023-06-12 14:59:42 -0300

    Update hunter-pto-BB.sh

--- hunter-pto-BB.sh
+++ hunter-pto-BB.sh
@@ -52,8 +52,8 @@ python3 ../tools/nmaptocsv/nmaptocsv.py -S -x "nmap.xml" -o "nmap.csv"
 
 #dirsearch
 echo ------------Init dirsearch------------
-wget -nc https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/common.txt
-#wget -nc https://gist.githubusercontent.com/jhaddix/b80ea67d85c13206125806f0828f4d10/raw/c81a34fe84731430741e0463eb6076129c20c4c0/content_discovery_all.txt
+#wget -nc https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/common.txt
+wget -nc https://gist.githubusercontent.com/jhaddix/b80ea67d85c13206125806f0828f4d10/raw/c81a34fe84731430741e0463eb6076129c20c4c0/content_discovery_all.txt
 dirsearch -u "$sitio" -w "$(pwd)/content_discovery_all.txt" -o "$(pwd)/dirnfiles.txt" --deep-recursive --force-recursive -e "zip,bak,log,xml,$extensiones" --format=csv -t 60
 rm content_discovery_all.txt
 
