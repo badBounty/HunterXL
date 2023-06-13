7071217555b71e5f2a38671d8826649469a806d1
Maxi <maxi1990maxi1990@gmail.com>
2023-06-12 21:04:23 -0300

    waf check added

--- hunter-pto-GB.sh
+++ hunter-pto-GB.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #Run:
-	#hunter-pto-BB.sh "https://www.example.com" "https://collaborator.com" "aspx,php,asp" "Cookie: galleta=valor; galleta2=valor"
-	#hunter-pto-BB.sh "https://www.example.com" "https://collaborator.com" "aspx,php,asp" "Authorization: Bearer JWT"
+	#hunter-pto-BB.sh "https://www.example.com" "https://collaborator.com" "aspx,php,asp" "true/false" "Cookie: galleta=valor; galleta2=valor"
+	#hunter-pto-BB.sh "https://www.example.com" "https://collaborator.com" "aspx,php,asp"  "true/false" "Authorization: Bearer JWT"
+	#hunter-pto-BB.sh "url site" "url callback" "extension" "force waf" "auth headers"
 
 #Output:
 	#nmap.csv
@@ -22,7 +23,8 @@
 sitio=$1
 callback=$2
 extensiones=$3
-galletas=$4
+forcewaf=$4
+galletas=$5
 
 if [ -z "${sitio}" ]; then
     echo "No se ha enviado el parametro sitio"
@@ -42,11 +44,32 @@ if [ -z "${extensiones}" ]; then
 	exit
 fi
 
+if [ -z "${wafcheck}" ]; then
+    echo "No se ha enviado el parametro waf check seleccione true o false"
+	echo "Usage: hunter-pto-BB.sh https://www.example.com https://collaborator.com \"aspx,php,asp\" \"true\" "
+	exit
+fi
 
 if [ -z "${galletas}" ]; then
     echo "No se ha enviado el parametro extensiones, las extensiones por defecto incluidas son zip,bak,log,xml"
 	echo "Usage: hunter-pto-BB.sh https://www.example.com https://collaborator.com \"aspx,php,asp\" \"Cookie: galleta=valor\""
 	exit
+fi
+
+WAF=0
+if [[  "$wafcheck" == "true" ]]
+	then
+		out=$(wafw00f "$sitio" | grep -e 'No WAF detected' -e 'was not detected' -c)
+		if [ $out -eq 1 ]
+		then
+			echo "No WAF detected in $sitio"
+		else
+			echo "The site $sitio is behind WAF"
+			wafw00f "$sitio"
+			WAF=1
+		fi
+else
+	echo "WAF check skipped, all test will be run"
 fi
 
 #creamos, puede fallar si existe en ese caso no nos interesa porque igual escribe ahi
@@ -61,14 +84,17 @@ sudo nmap --top-ports 1000 --open --script vuln,default,banner,ftp-anon,ftp-boun
 python3 ../tools/nmaptocsv/nmaptocsv.py -S -x "nmap.xml" -o "nmap.csv"
 
 #dirsearch
-echo ------------Init dirsearch------------
-wget -nc https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/common.txt
-#wget -nc https://gist.githubusercontent.com/jhaddix/b80ea67d85c13206125806f0828f4d10/raw/c81a34fe84731430741e0463eb6076129c20c4c0/content_discovery_all.txt
-dirsearch -u "$sitio" -w "$(pwd)/content_discovery_all.txt" -o "$(pwd)/dirnfiles.txt" --deep-recursive --force-recursive -e "zip,bak,log,xml,$extensiones" --format=csv -t 60 -H "$galletas"
-rm content_discovery_all.txt
+if [ $WAF -eq 0 ]
+	then
+		echo "------------Init dirsearch------------"
+		wget -nc https://gist.githubusercontent.com/jhaddix/b80ea67d85c13206125806f0828f4d10/raw/c81a34fe84731430741e0463eb6076129c20c4c0/content_discovery_all.txt
+		dirsearch -u "$sitio" -w "$(pwd)/content_discovery_all.txt" -o "$(pwd)/dirnfiles.txt" --deep-recursive --force-recursive -e "zip,bak,log,xml,$extensiones" --format=csv -t 60 -H "$galletas"
+		rm content_discovery_all.txt
+	else
+		echo "------------WAF Detected dirsearch skipped------------"
+fi
 
 #spidering
-
 echo "------------Init katana------------"
 katana -o katana.txt -silent -js-crawl -u "$sitio" -H "$galletas"
 
@@ -117,8 +143,13 @@ python3 ../retire-converter.py
 rm retirejs.json
 
 # xsstrike
-echo "------------Init xsstrike------------"
-python3 ../tools/XSStrike/xsstrike.py -u "$sitio" --crawl -l 3 --skip --headers "$galletas" | tee xsstrike.txt
+if [ $WAF -eq 0 ]
+	then
+		echo "------------Init xsstrike------------"
+		python3 ../tools/XSStrike/xsstrike.py -u "$sitio" --crawl -l 3 --skip --headers "$galletas" | tee xsstrike.txt
+	else
+		echo "------------WAF Detected xsstrike skipped------------"
+fi
 
 #creamos params
 echo "------------Creating params------------"
@@ -131,15 +162,27 @@ echo "start,vulnerable,end" > openredirect.csv
 cat params.txt | qsreplace $callback | sort | uniq | httpx -H "$galletas" -silent -status-code -location -json -fr | jq -r '. | .url + "," + .final_url' | awk -F, '{ print $1==$2?$1 "," $2 ",SI": $1 "," $2 ",NO" }' | grep "SI" >> openredirect.csv
 
 #SqlMap
-echo "------------Init sqlmap------------"
-sqlmap --banner --batch --level=1 --results-file=sqlmap.csv -m params.txt --ignore-redirects --headers="$galletas"
+if [ $WAF -eq 0 ]
+	then
+		echo "------------Init sqlmap------------"
+		sqlmap --banner --batch --level=1 --results-file=sqlmap.csv -m params.txt --ignore-redirects --headers="$galletas"
+	else
+		echo "------------WAF Detected SqlMap skipped------------"
+fi
 
 #dalfox
-echo "------------Init dalfox------------"
-wget -nc https://raw.githubusercontent.com/danielmiessler/SecLists/master/Fuzzing/XSS/XSS-Jhaddix.txt
-echo "<script>alert(1)</script>" >> XSS-Jhaddix.txt
-dalfox file params.txt --waf-evasion --output dalfox.json --format json --skip-mining-all --only-custom-payload --custom-payload ./XSS-Jhaddix.txt -H "$galletas"
-rm XSS-Jhaddix.txt
+if [ $WAF -eq 0 ]
+	then
+		echo "------------Init dalfox------------"
+		wget -nc https://raw.githubusercontent.com/danielmiessler/SecLists/master/Fuzzing/XSS/XSS-Jhaddix.txt
+		echo "<script>alert(1)</script>" >> XSS-Jhaddix.txt
+		dalfox file params.txt --waf-evasion --output dalfox.json --format json --skip-mining-all --only-custom-payload --custom-payload ./XSS-Jhaddix.txt -H "$galletas"
+		rm XSS-Jhaddix.txt
+		python3 ../dalfox-converter.py
+		rm dalfox.json
+	else
+		echo "------------WAF Detected dalfox skipped------------"
+fi
 
 python3 ../dalfox-converter.py
 rm dalfox.json
@@ -155,31 +198,24 @@ echo "--------------Init testssl--------------"
 testssl --csvfile testssl.csv "$sitio" 
 
 #Nikto
-echo "--------------Init nikto--------------"
-nikto -maxtime 15m -host "$sitio" -Format csv -output "./nikto.csv"
+if [ $WAF -eq 0 ]
+	then
+		echo "--------------Init nikto--------------"
+		nikto -maxtime 15m -host "$sitio" -Format csv -output "./nikto.csv"
+	else
+		echo "------------WAF DetectedniktoZAP skipped------------"
+fi
 
 #ZAP
-CSVHEADERS=1
-touch zap-runner.sh
-
-echo "echo \"--------------Init zap--------------\"" >> zap-runner.sh
-
-#This can be run from WSL or a real Linux
-if [[ $(cat /proc/version) == *"WSL"* ]]; then
-    echo "docker.exe run -v \"$(wslpath -w .)\":/zap/wrk owasp/zap2docker-stable zap-baseline.py -t $sitio -s -j -T 10 -m 5 -a -J  zap.json" >> zap-runner.sh
-else
-    echo "sudo docker run -v $(pwd):/zap/wrk owasp/zap2docker-stable zap-baseline.py -t $sitio -s -j -T 10 -m 5 -a -J zap.json"  >> zap-runner.sh
+if [ $WAF -eq 1 ]
+	then
+		echo "--------------Init zap--------------"
+		sudo docker run -v $(pwd):/zap/wrk owasp/zap2docker-stable zap-baseline.py -t $sitio -s -j -T 10 -m 5 -a -J zap.json
+		echo "--------------Init CSV Zap--------------"
+		python3 ../zap-converter-init.py
+		echo "--------------Init Converter JSON To CSV--------------"
+		python3 ../zap-converter.py
+	else
+		echo "------------WAF Detected ZAP skipped------------"
+		exit
 fi
-
-if [ $CSVHEADERS -eq 1 ]; then
-    echo "echo --------------Init CSV Zap"  >> zap-runner.sh
-    echo "python3 ../zap-converter-init.py" >> zap-runner.sh
-    CSVHEADERS=0
-fi
-
-echo "echo --------------Init Converter JSON To CSV--------------" >> zap-runner.sh
-echo "python3 ../zap-converter.py" >> zap-runner.sh
-echo "rm zap.json" >> zap-runner.sh		
-
-sh zap-runner.sh
-rm zap-runner.sh
