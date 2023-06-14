143b00ceaa1d5682a2f2516708b2d9d732334ff0
Maxi <maxi1990maxi1990@gmail.com>
2023-06-13 23:07:04 -0300

    fixed

--- hunter-pto-BB.sh
+++ hunter-pto-BB.sh
@@ -88,7 +88,7 @@ if [ $WAF -eq 0 ]
 	then
 		echo "------------Init dirsearch------------"
 		wget -nc https://gist.githubusercontent.com/jhaddix/b80ea67d85c13206125806f0828f4d10/raw/c81a34fe84731430741e0463eb6076129c20c4c0/content_discovery_all.txt
-		dirsearch -u "$sitio" -w "$(pwd)/content_discovery_all.txt" -o "$(pwd)/dirnfiles.txt" --deep-recursive --force-recursive -e "zip,bak,log,xml,$extensiones" --format=csv -t 60
+		dirsearch -u "$sitio" -w "$(pwd)/content_discovery_all.txt" -o "$(pwd)/dirnfiles.txt" --deep-recursive --force-recursive -e "log,xml,$extensiones" --format=csv -t 60
 		rm content_discovery_all.txt
 	else
 		echo "------------WAF Detected dirsearch skipped------------"
